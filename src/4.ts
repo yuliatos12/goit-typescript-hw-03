4539251b912ae821048998a063c59aba73bf2f72
yuliatos12 <yaremenko.yulia8@gmail.com>
2023-11-06 21:45:42 +0200

    +

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,53 @@
+class Key {
+private signature: number;
+
+constructor() {
+    this.signature = Math.random();
+}
+
+getSignature(): number {
+    return this.signature;
+}
+}
+
+class Person {
+    constructor(private key: Key) {
+    }
+
+    getKey(): Key {
+        return this.key;
+
+    }
+}
+
+abstract class House {
+    public door: boolean = false;
+    private tenants: Person[];
+
+    constructor(protected key: Key) {
+        this.key = key;
+      }
+
+      comeIn(person: Person) {
+        if (this.door) {
+            this.tenants.push(person);
+          }
+      }
+
+      abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    openDoor(key: Key) {
+        if (this.key.getSignature() === key.getSignature()) {
+            this.door = true;
+          } else {
+            this.door = false;
+          }
+    }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
